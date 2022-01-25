5d5f79969f337b267c34aa65415dba89dc3d5301
daisy035510 <43841944+daisy035510@users.noreply.github.com>
2022-01-25 23:51:17 +0900

    구글연동

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -27,6 +27,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
         OAuth2UserService delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
