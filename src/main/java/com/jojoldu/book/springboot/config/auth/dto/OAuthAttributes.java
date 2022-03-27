31ea38c6447513754608b0ce845a5bd4566e9d63
daisy035510 <43841944+daisy035510@users.noreply.github.com>
2022-03-27 20:01:59 +0900

    commit

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 @Getter
 public class OAuthAttributes {
-
     private Map<String, Object> attributes;
     private String nameAttributeKey;
     private String name;
@@ -17,8 +16,7 @@ public class OAuthAttributes {
     private String picture;
 
     @Builder
-    OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
-
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
@@ -27,6 +25,10 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -36,6 +38,18 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
