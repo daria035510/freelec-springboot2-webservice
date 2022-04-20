b0381938b2b886e6fee763761d15a2e110245894
daisy <daisy035510@gmail.com>
2022-04-20 19:26:26 +0900

    Update Application.java

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class Application { 
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 }
+   
